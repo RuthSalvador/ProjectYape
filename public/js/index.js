4d173d992f5f958f53ad90a1b187cf5dc05a6356
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-06-20 11:51:35 -0500

    router.get ok

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const render = (root,data) => {
+  root.empty();
+  const wrapper = $('<div class="wrapper"></div>');
+  /*wrapper.append(Header());
+  wrapper.append(Cartelera(data));*/
+  root.append(wrapper);
+};
+
+$( _ => {
+  getUsers((err,data) => {
+    if (err) console.log(err);
+
+    console.log(data);
+    const root = $("#root");
+    render(root,data);
+  });
+});
+
+
---
+++ public/js/models/users.js
@@ -0,0 +1,20 @@
+const getUsers = (callback) => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load',_ => {
+    if (xhr.status != 200) callback(new Error("Error al obtener datos"));
+    callback(null,xhr.response);
+  });
+
+  xhr.open('GET','api/users');
+  xhr.responseType = 'json';
+  xhr.send();
+};
+
+/*const getUser = (id,callback) => {
+  $.get('api/users/'+id,(data) => {
+    if (data.status != 200) callback(new Error("Error al obtener la peli"));
+    callback(null,data);
+  })
+};
+*/
